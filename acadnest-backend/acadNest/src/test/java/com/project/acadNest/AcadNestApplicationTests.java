9eab48356aea93e5a983d75c35af431e33693113
deepak728 <npe.deepak@gmail.com>
2025-01-05 16:29:40 +0530

    springboot init

---
+++ acadnest-backend/acadNest/src/test/java/com/project/acadNest/AcadNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.acadNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
